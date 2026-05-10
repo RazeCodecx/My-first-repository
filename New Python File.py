231eb01722a018ea747c149fe475f50e94982e94
RazeCodecx <rennyoliveros06@gmail.com>
2026-05-10 19:21:52 -0400

    ahi vamos

--- New Python File.py
+++ New Python File.py
@@ -1,1 +1,17 @@
-print ("hi git!!")
+# my first code in python
+print ("hi world") 
+print ('heyhey')
+
+
+print (type ("hey.variable"))
+print (type (5))
+print (type (1.5))
+print (type (True))
+print (type (1+2j))
+print ((5+2))
+
+
+
+"""
+this is a comment in python
+"""
